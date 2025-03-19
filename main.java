a20a970c53dad5cf761b6105ff7dca99fbbca9f6
GustaDev <gustagames0912@gmail.com>
2025-03-19 16:39:01 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,31 +1,42 @@
 class Dog {
     String name;
+    int age;
+    double weight;
 
-    public Dog (String name) {
+    public Dog (String name, int age, double weight) {
         this.name = name;
+        this.age = age;
+        this.weight = weight;
     }
 
     public void displayDog () {
         System.out.println("Dog name: " + name);
+        System.out.println("Dog age: " + age);
+        System.out.println("Dog weight: " + weight);
     }
 }
 
 class Cat extends Dog {
-    public Cat (String name) {
-        super(name);
+    public Cat (String name, int age, double weight) {
+        super(name, age, weight);
     }
 
     public void displayCat() {
-        System.out.println("I have a cat to! his name is " + name);
+        System.out.println("I have a cat to! his name is: " + name);
+        System.out.println("Cat age: " + age);
+        System.out.println("Cat Weight: " + weight);
+
     }
 }
 
-
 public class Main {
     public static void main(String[] args) {
-        Dog myDog = new Dog("Tico");
-        Cat myCat = new Cat("John");
+        Dog myDog = new Dog("Tico", 12, 14.4);
+        Cat myCat = new Cat("John", 4, 8);
         myDog.displayDog();
+
+        System.out.println();
+
         myCat.displayCat();
     }
 }
