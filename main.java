b2c8c4a11183f7fd9969a0ad25bc198634696ad5
GustaDev <gustagames0912@gmail.com>
2025-03-19 16:24:41 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,21 +1,31 @@
 class Dog {
     String name;
-    String color;
 
-    public Dog (String name, String color) {
+    public Dog (String name) {
         this.name = name;
-        this.color = color;
     }
 
     public void displayDog () {
-        System.out.println("Dog name: " + name + "Dog color: " + color);
+        System.out.println("Dog name: " + name);
+    }
+}
+
+class Cat extends Dog {
+    public Cat (String name) {
+        super(name);
+    }
+
+    public void displayCat() {
+        System.out.println("I have a cat to! his name is " + name);
     }
 }
 
 
 public class Main {
     public static void main(String[] args) {
-        Dog myDog = new Dog("Tico", "Brown");
+        Dog myDog = new Dog("Tico");
+        Cat myCat = new Cat("John");
         myDog.displayDog();
+        myCat.displayCat();
     }
 }
